1254ad844a43b9ff2f99483cdd558c9950eacbf9
Aleone44 <164150842+aleone44@users.noreply.github.com>
2024-11-21 11:10:35 +0100

    only check if it works befor any kind of modify

--- src/main/java/org/apache/commons/imaging/ColorTools.java
+++ src/main/java/org/apache/commons/imaging/ColorTools.java
@@ -42,7 +42,6 @@ import java.io.IOException;
  */
 public class ColorTools {
 
-
     public BufferedImage convertBetweenColorSpaces(BufferedImage bi, final ColorSpace from, final ColorSpace to) {
         final RenderingHints hints = new RenderingHints(RenderingHints.KEY_RENDERING, RenderingHints.VALUE_RENDER_QUALITY);
         hints.put(RenderingHints.KEY_COLOR_RENDERING, RenderingHints.VALUE_COLOR_RENDER_QUALITY);
